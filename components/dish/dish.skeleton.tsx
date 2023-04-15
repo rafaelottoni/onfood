4224f6c74427f69b04329d4f2570feeef3a45498
Rafael Ottoni <rafael.ottoni@engesoftware.com>
2023-04-15 06:54:56 -0300

    refactor(dish-skeleton): cleaner code for the iteration

--- components/dish/dish.skeleton.tsx
+++ components/dish/dish.skeleton.tsx
@@ -3,31 +3,27 @@ import "react-loading-skeleton/dist/skeleton.css";
 import { DishSkeletonProps } from "./types";
 
 export function DishSkeleton({ count }: DishSkeletonProps) {
-  const skeletons = [];
-
-  for (let i = 0; i < count; i++) {
-    skeletons.push(
-      <div>
+  const skeletons = Array.from({ length: count }, (_, index) => (
+    <div key={index}>
+      <Skeleton
+        count={1}
+        height={200}
+        width="100%"
+        style={{ marginBottom: "20px" }}
+      />
+      <div className="relative mb-1">
+        <Skeleton inline={true} height={24} width="40%" count={1} />
         <Skeleton
+          inline={true}
+          height={24}
+          width="20%"
           count={1}
-          height={200}
-          width="100%"
-          style={{ marginBottom: "20px" }}
+          style={{ marginLeft: "40%" }}
         />
-        <div className="relative mb-1">
-          <Skeleton inline={true} height={24} width="40%" count={1} />
-          <Skeleton
-            inline={true}
-            height={24}
-            width="20%"
-            count={1}
-            style={{ marginLeft: "40%" }}
-          />
-        </div>
-        <Skeleton count={1} height={24} width="50%" />
-      </div>,
-    );
-  }
+      </div>
+      <Skeleton count={1} height={24} width="50%" />
+    </div>
+  ));
 
   return (
     <SkeletonTheme baseColor="#ccc" highlightColor="#eee">
